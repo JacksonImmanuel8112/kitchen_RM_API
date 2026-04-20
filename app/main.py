c5cf2a95086c05f98eaca353e6f4405af710d9aa
jackson_Immanuel <105717601+JacksonImmanuel8112@users.noreply.github.com>
2026-04-20 12:42:02 +0530

    cors issue changes

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",
-        "http://127.0.0.1:4200"
+        "http://127.0.0.1:4200",
+        "https://kitchen-rm.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
